cb00ea467e7e1c8e753c77daab2c21e9fdf02abf
Collin Herber <rustaddicts@gmail.com>
2024-05-14 13:19:08 -0400

    chore: initial electron boilerplate push

---
+++ frontend-app/src/components/titlebar.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Nav from './nav.svelte';
+
+  // export let fileMenu = [];
+</script>
+<header id="titlebar">
+  <div class="title app-region">Electron template</div>
+  <Nav />
+</header>
