e473d03d4c9a995b4737f0917da28721bd1e43c0
Collin Herber <rustaddicts@gmail.com>
2024-05-14 13:53:32 -0400

    chore: update to svelte 4

--- frontend-app/src/components/transition.svelte
+++ frontend-app/src/components/transition.svelte
@@ -7,8 +7,8 @@
   });
 </script>
 {#if show}
-  <div in:fly={{x: -300, duration: 600}} out:fly={{y: -50, duration: 200}}><slot></slot></div>
-  <!-- <div in:fade={{delay: 1000, duration: 1000}} out:fade={{duration: 1000}}><slot></slot></div> -->
+  <div in:fly|global={{x: -300, duration: 600}} out:fly|global={{y: -50, duration: 200}}><slot></slot></div>
+  <!-- <div in:fade|global={{delay: 1000, duration: 1000}} out:fade|global={{duration: 1000}}><slot></slot></div> -->
   <!-- <div><slot></slot></div> -->
 {/if}
 <style>
